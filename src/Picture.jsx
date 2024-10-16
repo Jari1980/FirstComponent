6fa0466094d9eb891855992da6b161ea82817bd1
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-16 13:30:48 +0200

    Picture added and exported

---
+++ src/Picture.jsx
@@ -0,0 +1,12 @@
+
+
+function Picture(){
+  return(
+    <img 
+    src="/Forest.jpg"
+    alt="Forest"
+    />
+  )
+}
+
+export default Picture
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Picture from './Picture.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Picture />
   </StrictMode>,
 )
