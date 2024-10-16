418de7e16328a1acfae1382a1a75e98f3e77d066
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-16 13:40:15 +0200

    exercise2 done

---
+++ src/exercise2.jsx
@@ -0,0 +1,11 @@
+
+
+//In this exercise the paranthesis after retun were missing also semicolon after img needed to be removed
+//Or the whole row with img could have been placed after return in one line
+
+export default function Profile() {
+    return(
+      <img src="https://www...." alt="Exercise2" />
+    )
+  }
+  
